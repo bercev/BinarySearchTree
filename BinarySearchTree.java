bec979a8b931707ef855a48c7c881bccb0bd032a
Berat <140023449+bercev@users.noreply.github.com>
2024-07-19 23:00:00 -0700

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,5 +1,6 @@
 public class BinarySearchTree {
 
+    // Nodes used instead of array
     public class TreeNode {
         int data;
         TreeNode left;
@@ -19,15 +20,17 @@ public class BinarySearchTree {
             data = n.data;
         }
     }
-    int index = 0;
-    int numberOfLevels = 0;
-    TreeNode root;
-    Queue<TreeNode> mainList = new Queue();
+    int index = 0; // helps with printing
+    int numberOfLevels = 0; // determines how many levels there are to the tree
+    TreeNode root; // root node
+    Queue<TreeNode> mainList = new Queue(); // used with printing
 
+    // constructor
     public BinarySearchTree() {
         root = null;
     }
 
+    // determines the number of levels
     void countNumberOfLevels(int x) {
         int counter = 0;
         while (x != 0)
@@ -39,11 +42,13 @@ public class BinarySearchTree {
             numberOfLevels = counter;
     }
 
+    // inserts node in the bst
     void insert(int x)
     {
         int currentIndex = 0;
         System.out.println("\n--> insert: " + x);
 
+        // if root is null, place first node there
         if (root == null) {
             root = new TreeNode(x);
             System.out.println("Inserted @ index: " +  currentIndex);
@@ -51,12 +56,12 @@ public class BinarySearchTree {
             return;
         }
 
-        TreeNode temp = root;
-        TreeNode previous = null;
+        TreeNode temp = root; // used to iterate
+        TreeNode previous = null; // helps keep the root to the new node connected
         while (true)
         {
-            previous = temp;
-            if (x < temp.data)
+            previous = temp; //  sets it to iterator, keeping the present position and root connected
+            if (x < temp.data) // if less than root, go insert on the left side of the tree
             {
                 currentIndex = (2 * currentIndex) + 1;
                 System.out.println(" Left <<< " + currentIndex);
@@ -72,7 +77,7 @@ public class BinarySearchTree {
                 }
 
             }
-            else {
+            else { // if greater than root, go insert on the right side of the tree
                 currentIndex = (2 * currentIndex) + 2;
                 temp = temp.right;
                 System.out.println(" Right >>> " + currentIndex);
@@ -89,6 +94,7 @@ public class BinarySearchTree {
 
     }
 
+    // used with the previousInOrder method
     void Level(int x)
     {
         while (x != 0)
@@ -98,6 +104,7 @@ public class BinarySearchTree {
         }
     }
 
+    // previousInOrder method, to check if bst implementation was correct
     void previousInOrder(TreeNode node, int currentIndex) {
         if (node != null) {
             previousInOrder(node.left, 2*currentIndex + 1);
@@ -108,6 +115,7 @@ public class BinarySearchTree {
     }
 
 
+    // new inOrder method.
     void inOrder() {
         for (int i = 0; i < numberOfLevels+1; i++) {
             inputter();
@@ -115,26 +123,30 @@ public class BinarySearchTree {
         }
 
     }
+
+    // inputs each level of the tree into the mainList queue
     void inputter() {
-        if (index == 0) {
+        if (index == 0) { // if at first level, only push once
             mainList.Push(root);
             index++;
             return;
         }
+
         int length = mainList.GetLength();
-        Queue<TreeNode> newList = new Queue();
+        Queue<TreeNode> newList = new Queue(); // queue to hold the next level (mainList holds previous level)
         for (int i = 0; i < length; i++) {
-            if (mainList.Peek() == null) {
+            if (mainList.Peek() == null) { // if data is null, then push 2 nulls into the new list
                 newList.Push(null);
                 newList.Push(null);
                 mainList.Pop();
             } else {
-                newList.Push(mainList.Peek().left);
-                newList.Push(mainList.Peek().right);
-                mainList.Pop();
+                newList.Push(mainList.Peek().left); // pushes left into queue
+                newList.Push(mainList.Peek().right); // pushes right into queue
+                mainList.Pop(); // removes the head of the queue so the next one in line can .push into newList
             }
         }
 
+        // this code block refills the mainList in order to be used with printer()
         length = newList.GetLength();
         for (int i = 0; i < length; i++) {
             if (newList.Peek() == null) {
@@ -146,10 +158,12 @@ public class BinarySearchTree {
         }
 
     }
+
+    // prints out each level of the tree
     void printer() {
 
         int length = mainList.GetLength();
-        Queue<TreeNode> copy = new Queue();
+        Queue<TreeNode> copy = new Queue(); // copying the mainList to not modify it
         for (int i = 0; i < length; i++) {
             if (mainList.Peek() == null) {
                 copy.Push(null);
@@ -160,14 +174,14 @@ public class BinarySearchTree {
         }
 
         while(!copy.IsEmpty()) {
-            if (copy.Peek() == null) {
+            if (copy.Peek() == null) { // if null, represent it with a "-"
                 System.out.print("-\t");
             } else {
-                System.out.print(copy.Peek().data + "\t");
+                System.out.print(copy.Peek().data + "\t"); // else, print out data
             }
-            mainList.Push(copy.Pop());
+            mainList.Push(copy.Pop()); // refills the mainList back to normal, so it can work with insertter
         }
-        System.out.println("|");
+        System.out.println("|"); // end of level
     }
 
 
@@ -186,9 +200,22 @@ public class BinarySearchTree {
         bst.insert(63);
         bst.insert(65);
         System.out.println("\n\n\n\n==============================================");
-        System.out.println("InOrder");
+        System.out.println("Previous InOrder function");
         bst.previousInOrder(bst.root, 0);
-        System.out.println("\nInOrder v2 Horizontal Tree Level top-bottom");
+        System.out.println("\nNew InOrder function!");
         bst.inOrder();
+
+        System.out.println("\n\n\n\n==============================================");
+        System.out.println("Testing with random numbers in a new binary search tree");
+        BinarySearchTree treeTwo = new BinarySearchTree();
+        for (int i = 0; i < 10; i++) {
+            int random = (int) (Math.random() * 101); // generating random numbers 0-100
+            treeTwo.insert(random);
+        }
+        System.out.println("\n\n\n\n==============================================");
+        System.out.println("Previous InOrder function");
+        treeTwo.previousInOrder(treeTwo.root, 0);
+        System.out.println("\nNew InOrder function!");
+        treeTwo.inOrder();
     }
 }
